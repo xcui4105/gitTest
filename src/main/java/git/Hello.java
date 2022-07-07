bd7bafc6195b56b70d854ebe87d289e030aed69b
xc <1147410816@qq.com>
2022-07-07 23:52:32 +0800

    first version

---
+++ src/main/java/git/Hello.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author Hermione
+ * @create 07-07 23:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
