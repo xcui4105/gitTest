aa2ad116337ad9e77af5383c1b484abfbd94f67a
xc <1147410816@qq.com>
2022-07-08 09:27:08 +0800

    hot-fix-1 version

--- src/main/java/git/Hello.java
+++ src/main/java/git/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
 //        再切回来的时候，它会使用 git branch -d <branch-name> 的方式，再把临时创建的那个仅供回看目的的分支给删除掉
+        System.out.println("x cui");
     }
 }
